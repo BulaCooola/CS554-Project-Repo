19ddca039ded72f88b8086497870cbd8238e0bf9
Padraig Phelps <pphelps@stevens.edu>
2024-04-29 21:48:11 -0400

    bug

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,6 +1,5 @@
 export const mongoConfig = {
-  //"mongodb://127.0.0.1:27017/",
-  serverUrl:
-    "mongodb+srv://pphelps:TourneyPro@tourneypro.lzylpud.mongodb.net/?retryWrites=true&w=majority&appName=TourneyPro", //URL FOR CLOUD DB
+  serverUrl: "mongodb://127.0.0.1:27017/",
+
   database: "TourneyPro",
 };

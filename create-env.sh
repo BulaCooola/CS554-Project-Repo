230af3382433bba306ef34c3f71b1e5081456d8b
bbulatao <bbulatao@stevens.edu>
2024-04-30 19:26:26 -0400

    Removed front folder
    Co-Authored-By: Padraig-Phelps <92749064+Padraig-Phelps@users.noreply.github.com>
    Co-Authored-By: lennonokun <21065593+lennonokun@users.noreply.github.com>

--- create-env.sh
+++ create-env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Generating auth_secret..."
+echo "Generating nextauth_secret..."
 nextauth_secret=$(openssl rand -hex 32)
-echo "AUTH_SECRET=$nextauth_secret" > .env
+echo "NEXTAUTH_SECRET=$nextauth_secret" > .env
 echo ".env file created with AUTH_SECRET"

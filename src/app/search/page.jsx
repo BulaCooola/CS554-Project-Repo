a53d7ef27b3d6aad8e95b2778990c547476ac25e
bbulatao <bbulatao@stevens.edu>
2024-04-30 19:23:22 -0400

    bug fix - build error fixes
    Co-Authored-By: Padraig-Phelps <92749064+Padraig-Phelps@users.noreply.github.com>
    Co-Authored-By: lennonokun <21065593+lennonokun@users.noreply.github.com>

--- src/app/search/page.jsx
+++ src/app/search/page.jsx
@@ -1,36 +1,41 @@
-'use client'
-import {useFormState as useFormState} from 'react-dom';
-import {useState, useEffect} from 'react';
-import { search } from '@/app/actions';
-import Link from 'next/link';
+"use client";
+import { useFormState as useFormState } from "react-dom";
+import { useState, useEffect } from "react";
+import { search } from "@/app/actions";
+import Link from "next/link";
 const initialState = {
-  message: null
+  message: null,
 };
 
-function searchPage(props) {
+function SearchPage(props) {
   const [state, formAction] = useFormState(search, initialState);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-base">
       <form action={formAction} className="w-1/2 mx-auto my-20">
         <label className="input input-bordered flex items-center gap-2 w-full max-w-xs mx-auto my-2">
           Search:
-          <input name='search' id='search' type='text' placeholder="Search" required/>
+          <input name="search" id="search" type="text" placeholder="Search" required />
         </label>
-        <div className='form-group'>
-        <button className="btn btn-active btn-neutral flex mx-auto" type='submit'>
-          Search
-        </button>
+        <div className="form-group">
+          <button className="btn btn-active btn-neutral flex mx-auto" type="submit">
+            Search
+          </button>
         </div>
         {console.log(state)}
         {state && state.message && (
           <div>
             <h3>Results: </h3>
             <ul>
-              {state.message.map((result) => {
+              {state.message.map((result, index) => {
                 if (result.type) {
                   return (
-                    <li><Link href={`/${result.type}s/${result.data.id}`}>{result.type.charAt(0).toUpperCase() + result.type.slice(1)}: {result.data.name}</Link></li>
-                  )
+                    <li key={index}>
+                      <Link href={`/${result.type}s/${result.data.id}`}>
+                        {result.type.charAt(0).toUpperCase() + result.type.slice(1)}:{" "}
+                        {result.data.name}
+                      </Link>
+                    </li>
+                  );
                 }
               })}
             </ul>
@@ -41,4 +46,4 @@ function searchPage(props) {
   );
 }
 
-export default searchPage;
+export default SearchPage;

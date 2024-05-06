833a65eeba8e30d7cd647c889ba7d40d3ca8e0fa
bbulatao <bbulatao@stevens.edu>
2024-05-06 03:10:34 -0400

    New Seed File
    Seeds ~500 teams, ~5000 users, and 20 brackets
    
    Co-Authored-By: Mudkip101004 <47804620+Mudkip101004@users.noreply.github.com>
    Co-Authored-By: Padraig-Phelps <92749064+Padraig-Phelps@users.noreply.github.com>
    Co-Authored-By: lennonokun <21065593+lennonokun@users.noreply.github.com>

--- src/app/api/seed/route.js
+++ src/app/api/seed/route.js
@@ -271,33 +271,10 @@ export async function GET(req) {
       team.coachId,
       team.memberIds
     );
-    console.log("Added team:", teamObject);
+    console.log("Added team:", teamObject.name);
   }
 
-  const sportsCategories = [
-    "Baseball",
-    "Basketball",
-    "Cross Country",
-    "Fencing",
-    "Football",
-    "Golf",
-    "Gymnastics",
-    "Ice Hockey",
-    "Lacrosse",
-    "Soccer",
-    "Swimming and Diving",
-    "Tennis",
-    "Track and Field",
-    "Volleyball",
-    "Water Polo",
-    "Wrestling",
-    "Bowling",
-    "Field Hockey",
-    "Softball",
-    "Other",
-  ];
-
-  const generateTeamName = () => {
+  const generateTeamName = async () => {
     const adjectives = [
       "Awesome",
       "Fantastic",
@@ -347,20 +324,198 @@ export async function GET(req) {
     return `${adjective} ${noun}`;
   };
 
-  let numberOfTeamsPerSport = 32;
+  const generateFirstName = async () => {
+    const firstNames = [
+      "John",
+      "Michael",
+      "Robert",
+      "William",
+      "David",
+      "Richard",
+      "Joseph",
+      "Charles",
+      "Thomas",
+      "Christopher",
+      "Daniel",
+      "Matthew",
+      "Anthony",
+      "Mark",
+      "Donald",
+      "Paul",
+      "Steven",
+      "Andrew",
+      "Kenneth",
+      "Joshua",
+      "George",
+      "Kevin",
+      "Brian",
+      "Edward",
+      "Ronald",
+      "Timothy",
+      "Jason",
+      "Jeffrey",
+      "Ryan",
+      "Gary",
+      "Nicholas",
+      "Eric",
+      "Stephen",
+      "Jacob",
+      "Larry",
+      "Frank",
+      "Jonathan",
+      "Scott",
+      "Justin",
+      "Brandon",
+      "Raymond",
+      "Gregory",
+      "Samuel",
+      "Benjamin",
+      "Patrick",
+      "Jack",
+      "Dennis",
+      "Jerry",
+      "Alexander",
+      "Patrick",
+    ];
+    const firstName = firstNames[Math.floor(Math.random() * firstNames.length)];
+    return `${firstName}`;
+  };
+
+  const generateLastName = async () => {
+    const lastNames = [
+      "Doe",
+      "Smith",
+      "Williams",
+      "Brown",
+      "Jones",
+      "Miller",
+      "Davis",
+      "Garcia",
+      "Rodriguez",
+      "Martinez",
+      "Hernandez",
+      "Lopez",
+      "Gonzalez",
+      "Wilson",
+      "Anderson",
+      "Thomas",
+      "Taylor",
+      "Moore",
+      "Jackson",
+      "Martin",
+      "Lee",
+      "Perez",
+      "Thompson",
+      "White",
+      "Harris",
+      "Sanchez",
+      "Clark",
+      "Ramirez",
+      "Lewis",
+      "Robinson",
+      "Walker",
+      "Young",
+      "Allen",
+      "King",
+      "Wright",
+      "Scott",
+      "Torres",
+      "Nguyen",
+      "Hill",
+      "Flores",
+      "Green",
+      "Adams",
+      "Nelson",
+      "Baker",
+      "Hall",
+      "Rivera",
+      "Campbell",
+      "Mitchell",
+    ];
+    const lastName = lastNames[Math.floor(Math.random() * lastNames.length)];
+    return `${lastName}`;
+  };
+
+  const generateUsername = async (firstName, lastName) => {
+    const sanitizedFirstName = firstName.toLowerCase().trim();
+    const sanitizedLastName = lastName.toLowerCase().trim();
+
+    const username = sanitizedFirstName.charAt(0) + sanitizedLastName;
+
+    return username;
+  };
+
+  const sportsCategories = [
+    "Baseball",
+    "Basketball",
+    "Cross Country",
+    "Fencing",
+    "Football",
+    "Golf",
+    "Gymnastics",
+    "Ice Hockey",
+    "Lacrosse",
+    "Soccer",
+    "Swimming and Diving",
+    "Tennis",
+    "Track and Field",
+    "Volleyball",
+    "Water Polo",
+    "Wrestling",
+    "Bowling",
+    "Field Hockey",
+    "Softball",
+    "Other",
+  ];
+
   const createTeams = async () => {
     try {
       for (const sport of sportsCategories) {
-        console.log(sport);
+        let numberOfTeamsPerSport = 32;
+        let teamIdList = [];
+        if (
+          sport === "Golf" ||
+          sport === "Bowling" ||
+          sport === "Other" ||
+          sport === "Gymnastics" ||
+          sport === "Tennis"
+        ) {
+          numberOfTeamsPerSport = 4;
+        }
         for (let i = 0; i < numberOfTeamsPerSport; i++) {
-          const teamName = generateTeamName();
+          const teamName = await generateTeamName();
           const team = {
             name: teamName,
             sport: sport,
             country: "United States of America",
-            coachId: paddy._id.toString(),
-            memberIds: [paddy._id.toString()],
+            coachId: undefined,
+            memberIds: [],
           };
+
+          for (let j = 0; j < 10; j++) {
+            const playerFirstName = await generateFirstName();
+            const playerLastName = await generateLastName();
+            const playerUsername = await generateUsername(playerFirstName, playerLastName);
+            const playerEmail = `${playerUsername}@stevens.edu`;
+            const playerPhoneNumber = "1234567890";
+
+            // Create player object
+            const playerObject = await userData.addUser(
+              playerUsername,
+              playerFirstName,
+              playerLastName,
+              playerEmail,
+              playerPhoneNumber,
+              hashedPassword
+            );
+
+            // push first person as coach
+            team.coachId = playerObject._id.toString();
+            // push into members list
+            team.memberIds.push(playerObject._id.toString());
+
+            console.log("Added player:", playerObject.username);
+          }
           const teamObject = await teamData.createTeam(
             team.name,
             team.sport,
@@ -368,10 +523,23 @@ export async function GET(req) {
             team.coachId,
             team.memberIds
           );
-          console.log("Added team:", teamObject);
+          teamIdList.push(teamObject._id.toString());
+          console.log(`Added team: ${teamObject.name} ${teamObject.sport}`);
         }
+
+        const bracket = await bracketData.createBracket(
+          `${sport} Bracket`,
+          "The quick brown fox jumps over the lazy dog.",
+          "5/30/2024",
+          "6/03/2024",
+          paddy._id.toString(),
+          sport,
+          numberOfTeamsPerSport,
+          teamIdList
+        );
+        console.log(`Added ${sport} Bracket`);
       }
-      console.log("Teams created successfully");
+      console.log("Generic Teams/Players/Brackets created successfully");
     } catch (error) {
       console.error("Error creating teams:", error);
     }
@@ -424,7 +592,6 @@ export async function GET(req) {
   //   "14"
   // );
   // console.dir(winner3.matches, { depth: null });
-
   await closeConnection();
   console.log("Done seeding database");
   return NextResponse.json({ done: true }, { status: 200 });

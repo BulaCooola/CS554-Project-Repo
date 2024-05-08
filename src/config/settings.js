98bef12b1d3c38fdd7db61df3cdb2ce5b84e9f44
Padraig Phelps <pphelps@stevens.edu>
2024-05-07 21:02:16 -0400

    redis + mongo

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,5 +1,6 @@
 export const mongoConfig = {
-  serverUrl: "mongodb://127.0.0.1:27017/",
+  serverUrl:
+    "mongodb+srv://pphelps:TourneyPro1@tourneypro.lzylpud.mongodb.net/?retryWrites=true&w=majority&appName=TourneyPro",
 
   database: "TourneyPro",
 };
